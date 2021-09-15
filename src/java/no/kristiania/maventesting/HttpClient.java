cbe8eb2f2d3c6e00593e15e4e1b7fa15433f75b4
Daniel Lysak <Daniel_kn@live.no>
2021-09-15 15:34:33 +0200

    fixed http request

--- src/java/no/kristiania/maventesting/HttpClient.java
+++ src/java/no/kristiania/maventesting/HttpClient.java
@@ -2,7 +2,6 @@ package no.kristiania.maventesting;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class HttpClient {
@@ -12,28 +11,27 @@ public class HttpClient {
 
     public HttpClient(String host, int port, String requestTarget) throws IOException {
         Socket socket = new Socket(host, port);
-        socket.getOutputStream().write(
-                ("GET /html HTTP/1.1 \r\n "+
-                        "Host: httpbin.org\r\n"+
-                        "\r\n").getBytes()
-        );
+        String request = "GET "+requestTarget+" HTTP/1.1\r\n"+
+                "Host:"+host+"\r\n"+
+                "Connection: close\r\n"+
+                "\r\n";
+
+        socket.getOutputStream().write(request.getBytes());
 
         StringBuilder result = new StringBuilder();
         String myResponseMessage = result.toString();
         System.out.println(myResponseMessage);
-        String statusLine = readLine(socket);
+        String statusLine = readFirstLine(socket);
         this.statusCode = Integer.parseInt(statusLine.split(" ")[1]);
-        //System.out.print(result);
-
-
     }
 
-    private String readLine(Socket socket) throws IOException {
+    private String readFirstLine(Socket socket) throws IOException {
         StringBuilder result = new StringBuilder();
         InputStream in = socket.getInputStream();
         int c;
         while ( (c = in.read()) != '\r'  ){
             result.append((char) c);
+
         }
         return result.toString();
     }
